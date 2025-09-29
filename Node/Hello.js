1da82168ddf62c922269465060abf76b5eee5e1c
Deokdragon <ejrdyd3899@gmail.com>
2025-09-29 14:11:43 +0900

    [feat] 서버 안열리는 버그 수정 및 서버 연결

--- Node/Hello.js
+++
@@ -1,9 +0,0 @@
-let http = require("http");
-http.createServer(function (request,response)
-{
-    response.writeHead(200, {'Conten-Type' : 'text/plain'});
-
-    response.end("Hello world");
-}).listen(8000);
-
-console.log("Server running at http://127.0.0.1:8000");
