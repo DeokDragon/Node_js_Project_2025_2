8b09dd510e036c7c4ffa9e9a78139fa936c71432
Deokdragon <ejrdyd3899@gmail.com>
2025-10-13 12:05:12 +0900

    [feat] 로그인 및 암호화 처리 (JWT)

---
+++ Unity/Assets/Scripts/AuTH/AuthManager.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text;
+using UnityEngine.Networking;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+public class AuthManager : MonoBehaviour
+{
+    private const string SERVER_URL = "http://localhost:4000";
+    private const string ACCESS_TOKEN_PREFS_KEY = "AccessToekn";
+    private const string REFRESH_TOKEN_PREFS_KEY = "RefreshToekn";
+    private const string TOKEN_EXPIRY_PREDS_KEY = "TokenExpiry";
+
+    private string accessToken;
+    private string refreshToken;
+    private DateTime tokenExpiryTime;
+
+    void Start()
+    {
+        LoadTokenFromPrefs();
+    }
+
+    private void LoadTokenFromPrefs()
+    {
+        accessToken = PlayerPrefs.GetString(ACCESS_TOKEN_PREFS_KEY, "");
+        refreshToken = PlayerPrefs.GetString(REFRESH_TOKEN_PREFS_KEY, "");
+        long expriyTicks = Convert.ToInt64(PlayerPrefs.GetString(TOKEN_EXPIRY_PREDS_KEY, ""));
+        tokenExpiryTime = new DateTime(expriyTicks);
+    }
+
+    private void SaveTokenToPrefs(string accessToken, string refreshToken, DateTime expiryTime)
+    {
+        PlayerPrefs.SetString(ACCESS_TOKEN_PREFS_KEY, accessToken);
+        PlayerPrefs.SetString(REFRESH_TOKEN_PREFS_KEY, refreshToken);
+        PlayerPrefs.SetString(TOKEN_EXPIRY_PREDS_KEY, expiryTime.Ticks.ToString());
+
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.tokenExpiryTime = expiryTime;
+    }
+
+    public IEnumerator Register(string username, string password)
+    {
+        var user = new {username, password};
+        var jsonData = JsonConvert.SerializeObject(user);
+
+        using (UnityWebRequest www = UnityWebRequest.PostWwwForm($"{SERVER_URL}/register", "POST"))
+        {
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonData);
+            www.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            www.downloadHandler = new DownloadHandlerBuffer();
+            www.SetRequestHeader("Content-Type", "application/json");
+
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                UnityEngine.Debug.Log($"Registration Error : {www.error}");
+            }
+            else
+            {
+                UnityEngine.Debug.Log("Registration successful");
+            }
+                    
+        }
+
+
+    }
+
+    public IEnumerator login(string username, string password)
+    {
+        var user = new { username, password };
+        var jsonData = JsonConvert.SerializeObject(user);
+
+        using (UnityWebRequest www = UnityWebRequest.PostWwwForm($"{SERVER_URL}/login", "POST"))
+        {
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonData);
+            www.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            www.downloadHandler = new DownloadHandlerBuffer();
+            www.SetRequestHeader("Content-Type", "application/json");
+
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                UnityEngine.Debug.Log($"Registration Error : {www.error}");
+            }
+            else
+            {
+                var respone = JsonConvert.DeserializeObject<LoginResponse>(www.downloadHandler.text);
+                UnityEngine.Debug.Log(respone);
+                SaveTokenToPrefs(respone.accessToken, respone.refreshToken, DateTime.UtcNow.AddMinutes(15));
+                UnityEngine.Debug.Log("Login Successful");
+            }
+
+        }
+
+
+    }
+}
+
+[System.Serializable]
+
+public class LoginResponse
+{
+    public string accessToken;
+    public string refreshToken;
+}
+
+[System.Serializable]
+
+public class RefreshTokenResponse
+{
+    public string accessToken;
+}
+
